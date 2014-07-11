45c29835f9b345ca0b21c27bb7916ecb3666c2ce
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-07-11 15:12:25 +0200

    added reset button in dataset

--- app/assets/javascripts/resources/querybuilder.js
+++ app/assets/javascripts/resources/querybuilder.js
@@ -1,17 +1,23 @@
 if (typeof QueryBuilder == 'undefined') {
-	QueryBuilder = {};
+    QueryBuilder = {};
 }
 
 QueryBuilder = {
-	selected_dataset : function(dataset){
-		$("#div_qb_select_class").show("fast");
-		$("#dd_select_dataset").hide("fast");
-		$("#div_step_info_dataset").hide("fast");
-		QueryBuilder.select_body($("#div_select_dataset"),dataset);
-	},
-	select_body : function(element,body){
-		element.find(".select-body").first().html(body);
-		console.log(element.find(".select-body").first());
-		element.show("fast");
-	}
+    selected_dataset : function(dataset){
+        $("#div_qb_select_class").show("fast");
+        $("#dd_select_dataset").hide("fast");
+        $("#div_step_info_dataset").hide("fast");
+        QueryBuilder.select_body($("#div_select_dataset"),dataset);
+    },
+    select_body : function(element,body){
+        element.find(".select-body").first().html(body);
+        console.log(element.find(".select-body").first());
+        element.show("fast");
+    },
+    reset_dataset : function(){
+        $(".done-dataset").hide("fast");
+        $("#div_qb_select_class").hide("fast");
+        $(".clear-dataset").show("fast");
+    }
 };
+    
