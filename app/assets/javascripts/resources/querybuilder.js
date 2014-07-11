aa4fed5a9508a8143145a2d4da2677a3788bc0f5
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-07-11 13:55:50 +0200

    added selection of dataset

--- app/assets/javascripts/resources/querybuilder.js
+++ app/assets/javascripts/resources/querybuilder.js
@@ -5,5 +5,13 @@ if (typeof QueryBuilder == 'undefined') {
 QueryBuilder = {
 	selected_dataset : function(dataset){
 		$("#div_qb_select_class").show("fast");
+		$("#dd_select_dataset").hide("fast");
+		$("#div_step_info_dataset").hide("fast");
+		QueryBuilder.select_body($("#div_select_dataset"),dataset);
+	},
+	select_body : function(element,body){
+		element.find(".select-body").first().html(body);
+		console.log(element.find(".select-body").first());
+		element.show("fast");
 	}
 };
