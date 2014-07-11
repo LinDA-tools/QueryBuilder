153c00ec5e549721c3f26b2a03c858fc2695862e
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-07-11 12:48:35 +0200

    added a sample dropdown list

--- app/helpers/query_helper.rb
+++ app/helpers/query_helper.rb
@@ -2,4 +2,12 @@ module QueryHelper
 	def get_uri(url)
 		return URI.encode(url.gsub("&lt;","<").gsub("&gt;",">"))
 	end
+
+	# This method lists the available datasets to query the data
+	#return <Hashmap> 
+	def get_datasets
+		datasets = {}
+		datasets["DBPedia"] = "http://dbpedia.org/sparql"
+		return datasets
+	end
 end
