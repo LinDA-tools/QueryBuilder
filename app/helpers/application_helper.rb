1139868a5f06b668c3ffb7d45701da45f2e8d3ae
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-07-11 22:08:52 +0200

    added actual free text search of classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def get_sparql_prefixes
 		prefix = ""
-		prefix += "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>."
-		prefix += "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>."
-		prefix += "PREFIX owl:<http://www.w3.org/2002/07/owl#>."
+		prefix += "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+		prefix += "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> "
+		prefix += "PREFIX owl:<http://www.w3.org/2002/07/owl#> "
 	end
 end
--- app/helpers/query_helper.rb
+++ app/helpers/query_helper.rb
@@ -18,22 +18,26 @@ module QueryHelper
 	#This method searches for classes in a dataset matching the search string
 	#return <Hashmap>
 	def search_classes(dataset,search_str)
-		classes = {}
-		classes["Actor"] = {}
-		classes["Country"] = {}
-		classes["Boxer"] = {}
+		classes = []
 
-		queryable = RDF::Repository.load(dataset)
+		#queryable = RDF::Repository.load(dataset)
 		#query = get_sparql_prefixes
-		query = "SELECT distinct ?a ?label "
-		query += " WHERE { ?a rdf:type owl:Class. ?a rdfs:label ?label. "
-		query += " FILTER(bound(?label) && langMatches(lang(?label), \"EN\") && REGEX(?label, '"+search_str.downcase+"'))}"
+		#query += "SELECT distinct ?a ?label "
+		#query += " WHERE { ?a rdf:type owl:Class. ?a rdfs:label ?label. "
+		#query += " FILTER(bound(?label) && langMatches(lang(?label), \"EN\") && REGEX(?label, '"+search_str.downcase+"'))}"
 		#sse = SPARQL.parse(query)
 		#results = []
 		#queryable.query(sse) do |result|
 		#  results << result
 		#end
-		results = SPARQL.execute(query, queryable)
+		#results = SPARQL.execute(query, queryable)
+		uri = get_uri("http://localhost:8080/rdf2any/v1.0/builder/classes?search="+search_str.downcase+"&dataset="+dataset)
+   		response = HTTParty.get(uri)
+   		unless response["results"]["bindings"].blank?
+   			response["results"]["bindings"].each do |result|
+   				classes << {:uri=>result["class"]["value"], :name=>result["label"]["value"].capitalize}
+   			end
+   		end
 		classes
 	end
 end
